9dc6326cffe227c4561beac023b56097ab1d22d6
anishsaganish <43968361+anishsaganish@users.noreply.github.com>
2018-10-28 19:27:17 -0700

    Update feature1.sql

--- ebia/ebia/feature1.sql
+++ ebia/ebia/feature1.sql
@@ -4,6 +4,7 @@
     [Name_of_Feature] NCHAR(10) NULL, 
     [Code_of_feature] NCHAR(10) NULL
 	 [dollar_of_feature] NCHAR(10) NULL,
-	
+-- add to feature three
+	 [Amount_of_feature] NCHAR(10) NULL
 )
 
