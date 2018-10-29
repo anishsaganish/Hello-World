724cd7062eb32481b874d15397747c554c8f711c
anishsaganish <43968361+anishsaganish@users.noreply.github.com>
2018-10-28 20:50:08 -0500

    Update feature1.sql

--- ebia/ebia/feature1.sql
+++ ebia/ebia/feature1.sql
@@ -3,5 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name_of_Feature] NCHAR(10) NULL, 
     [Code_of_feature] NCHAR(10) NULL
-	 [dollar_of_feature] NCHAR(10) NULL
+	 [dollar_of_feature] NCHAR(10) NULL,
+	 [Amount_of_feature] NCHAR(10) NULL
 )
+
