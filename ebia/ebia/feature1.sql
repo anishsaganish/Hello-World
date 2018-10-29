64e8088885fa1ea2cf95dc6e14bc30eb628a46c7
anishsaganish <43968361+anishsaganish@users.noreply.github.com>
2018-10-28 20:40:50 -0500

    Update feature1.sql

--- ebia/ebia/feature1.sql
+++ ebia/ebia/feature1.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[feature1]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name_of_Feature] NCHAR(10) NULL, 
-    [Code_of_feature] NCHAR(10) NULL
+    [Name_of_Feature] VARCHAR(10) NULL, 
+    [Code_of_feature] VARCHAR(10) NULL,
+	 [Sale_of_feature] VARCHAR(10) NULL
 )
